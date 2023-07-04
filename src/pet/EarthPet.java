9ff9bfd2a852ccd06400eb8e4bfd32139367c3c2
samuel <samugit.projects@gmail.com>
2023-07-04 13:08:49 -0300

    Aula11 A influência do modificador final na herança

--- src/pet/EarthPet.java
+++ src/pet/EarthPet.java
@@ -7,8 +7,9 @@ public class EarthPet extends Pet {
 
     }
 
-    public void talk(){
+    public void talk(){ // se acrescentar o modificador final em métodos não poderá ser sobrescrever em subclasses
         System.out.println("EarthPet talk");
+        // as situações para uso do final são raras e via de regra não é usado para restrição
 
     }
 }
--- src/pet/Pet.java
+++ src/pet/Pet.java
@@ -1,7 +1,11 @@
 package pet;
 @SuppressWarnings("ALL")
-public class Pet {
+public class Pet { // se acrescentar o modificador final em classes não poderá ser extendida em subclasses
     private String color;
+    // as situações para uso do final são raras e via de regra não é usado para restrição
+    // ficar atento para situações específicas com o conceito, por exemplo, de sobrescrever uma classe
+    // que no caso da classe String do Java, ver documentação, é definida como final e tem um corportamento bem específico
+    // por isso não pode herdar de String
 
     public String getColor(){
         return color;
