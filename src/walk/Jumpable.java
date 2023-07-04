d28e85aa77d2bf39979f06c119bbdd4ccc2aeffe
samuel <samugit.projects@gmail.com>
2023-07-04 14:06:36 -0300

    Aula16 Visibilidade métodos definidos por interface

--- src/walk/Jumpable.java
+++ src/walk/Jumpable.java
@@ -3,5 +3,9 @@ package walk;
 @SuppressWarnings("ALL")
 public interface Jumpable {
 
+    // todos os métodos definidos em interfaces são automaticamente public e abstract
+    // por isso não há necessidade de grafar isso na assinatura do método e as classes devem manter public explicitamente
+    // porque há uma regra que tanto na herança, quanto na implementação, não se pode diminuir o nível de visibilidade
+    // deixar sem o public na classe que está implementado a interface vai reduzir a visibilidade para package private
     void jump();
 }
