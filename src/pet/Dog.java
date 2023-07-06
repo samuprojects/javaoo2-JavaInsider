5f4e601283674595f8d66c9035369658a2fc1558
samuel <samugit.projects@gmail.com>
2023-07-06 13:10:34 -0300

    Aula25 Regras da implementação de métodos abstratos

--- src/pet/Dog.java
+++ src/pet/Dog.java
@@ -6,9 +6,20 @@ public class Dog extends EarthPet {
         System.out.println("Au-Au");
     }
 
-    // essa sobrescrita se tornou obrigatória por conta da classe Pet ter mudado para abstract
+    // com a implementação do sleep pela classe EarthPet Dog pode assumiro uso método por herança
+    // fica opcional sobrescrever para um novo comportamento, porém aqui só fica obrigatório
+    // se a implementação da superclasse EarthPet for removida, aí Dog precisa obrigatoriamente implementar
+    // isso ocorre porque de alguma forma a classe concreta (ou seja, aquela que instancia) precisa implementar
+    // seja por herança ou por ela mesma o método abstrato da classe Pet
     @Override
     public void sleep(int time) {
+        System.out.println("Dog sleeping");
 
     }
+
+    // outro ponto é que não existe métodos abstratos em classes não abstratas
+    // para criar métodos abstratos em sua classe ela precisa virar abstract
+    // porém o contrárionão é verdadeiro, pois uma classe abstrata pode ter métodos não abstratos
+    // porque no final ela continua sendo uma classe, podendo ter atributos, construtores, etc.
+    // public abstract au(); // não compila se remover o comentário do código
 }
--- src/pet/EarthPet.java
+++ src/pet/EarthPet.java
@@ -2,13 +2,28 @@ package pet;
 
 public abstract class EarthPet extends Pet { // Assim como em Pet não faz sentido instanciar EarthPet por isso abstract
 
-    public void walk(){
-        System.out.println("EarthPet walk");
+    public void walk(){ System.out.println("EarthPet walk"); }
 
-    }
+    public void talk(){ System.out.println("EarthPet talk"); }
 
-    public void talk(){
-        System.out.println("EarthPet talk");
-
+    // com essa implementação as subclasses não são obrigadas a implementar por si mesmas
+    // pois a recebem por herança
+    // abaixo de Pet precisa constar essa implementação OU aqui OU nas classes herdeiras mais abaixo
+    @Override
+    public void sleep(int time) {
+        System.out.println("EarthPet sleeping");
     }
 }
+
+// comentário do Tadeu a respeito sobre uma dúvida em usar interface ou classe abstrata
+//  interface é basicamente um contrato de implementação, ou seja, obrigatoriamente
+//  você precisa implementar todos os métodos declarados numa interface,
+//  só não é obrigatório implementar os métodos que utilizam o “default“ na assinatura,
+//  pois para eles é possível dar uma implementação dentro da própria interface,
+//  já nas classes abstratas, você só precisa implementar de forma obrigatória
+//  os métodos abstratos, sendo que os demais métodos, que são concretos,
+//  são sobrescritos apenas de forma opcional caso forem necessários.
+//  Em se tratando de classes abstratas você pode “escolher“ os métodos que irá utilizar
+//  à menos que eles sejam abstratos. Na interface você deve obrigatoriamente implementar
+//  todos os métodos não “default“. Quanto aos “default“, você pode escolher se vai ou não utilizá-los
+
