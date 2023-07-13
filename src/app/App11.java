1f8b0638bfd42b824838dce6f59129548d34e868
samuel <samugit.projects@gmail.com>
2023-07-13 18:36:10 -0300

    Aula35 Criando uma classe para representar dados

---
+++ src/app/App11.java
@@ -0,0 +1,13 @@
+package app;
+
+import record.Person;
+
+public class App11 {
+
+    public static void main(String[] args) {
+
+        Person person = new Person("José","Silva", 30);
+        System.out.println(person);
+
+    }
+}
---
+++ src/record/Person.java
@@ -0,0 +1,51 @@
+package record;
+
+import java.util.Objects;
+
+@SuppressWarnings("ALL")
+public class Person {
+
+    private final String firstName;
+    private final String lastName;
+    private final int age;
+
+    public Person(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return age == person.age && Objects.equals(firstName, person.firstName) && Objects.equals(lastName, person.lastName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, lastName, age);
+    }
+}
